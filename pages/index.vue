0b67e9a4f28caa86c14ec15c8e093b52c3c8fa7c
S3Prototype <liuqahs15@gmail.com>
2021-08-13 17:20:53 -0400

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Products:</h1>
+    <div v-if="getProductList">          
+      <ul>
+        <li v-for="product in getProductList.items" :key="product._id">
+          {{ product.name }}: {{ product.price }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      Testing...
+    </div>
+  </div>
+</template>
+
+<script>
+import getProductList from '../apollo/queries/getProductList.gql'
+export default {
+    apollo: {
+      getProductList: {
+        query: getProductList
+      }
+    }
+  }
+</script>
